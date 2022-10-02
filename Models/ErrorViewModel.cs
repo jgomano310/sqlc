3c53bf46d78987ef7be18ad93e62544e51e98c0c
Regino Fernández García <rfg@altair.edu.es>
2022-10-02 19:07:54 +0200

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pruebaConexionPostgreSQLV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
